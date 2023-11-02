d634c96a2e74407a9d9098e7404d057e061d1716
bartosz12-12 <bartek.fi@interia.pl>
2023-11-02 11:14:23 +0100

    dodanie tabel i zmiany w pliku admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Person)
+class TabPerson(admin.ModelAdmin):
+    list_display = ['id', 'name','surname','sex','date_of_birth']
+
+class TabRKebab(admin.ModelAdmin):
+    list_display = ['id', 'person_display','restaurant_display','meat','sauce','vegetables','additions']
+
+    @admin.display(description='person')
+    def person_display(self, obj):
+        return f'{obj.person.name} {obj.person.surname}'
+
+    @admin.display(description='restaurant')
+    def restaurant_display(self, obj):
+        return f'{obj.restaurant.name}'
+
+class TabRestaurant(admin.ModelAdmin):
+    list_display = ['id', 'name','country','city','street','typeRestaurant']
+admin.site.register(Person,TabPerson)
+admin.site.register(Restaurant,TabRestaurant)
+admin.site.register(RatingKebaba,TabRKebab)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+def validate_rating(value):
+    if value < 1 or value > 10:
+        raise ValidationError("Ocena mięsa musi być w skali od 1 do 10.")
 
 class Person(models.Model):
     class Plec(models.IntegerChoices):
@@ -10,12 +15,20 @@ class Person(models.Model):
     sex = models.IntegerField(choices=Plec.choices)
     date_of_birth = models.DateField()
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Restaurant(models.Model):
+    class Type(models.IntegerChoices):
+        KEBAB = 1
+        ZAPIEKANKA = 2
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    street = models.CharField(max_length=100)
+    typeRestaurant = models.IntegerField(choices=Type.choices)
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class RatingKebaba(models.Model):
+    person = models.ForeignKey(Person,on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+    meat =  models.IntegerField(default=0, help_text="Ocena miesa w skali od 1 do 10",validators = [validate_rating])
+    sauce = models.IntegerField(default=0, help_text="Ocena sosu w skali od 1 do 10",validators = [validate_rating])
+    vegetables = models.IntegerField(default=0, help_text="Ocena warzyw w skali od 1 do 10",validators = [validate_rating])
+    additions = models.IntegerField(default=0, help_text="Ocena dodatkow w skali od 1 do 10",validators = [validate_rating])
